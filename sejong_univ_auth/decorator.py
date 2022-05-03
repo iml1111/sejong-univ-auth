1248b33a621e372b8c847caf9ec968395fd0f6cf
IML <shin10256@gmail.com>
2022-05-03 15:33:05 +0900

    timeout 데코레이터 구현

--- sejong_univ_auth/decorator.py
+++ sejong_univ_auth/decorator.py
@@ -1,3 +1,4 @@
+from requests.exceptions import Timeout
 from functools import wraps
 
 
@@ -9,3 +10,13 @@ def header(key: str, value: str):
             return func(self, *args, **kwargs)
         return wrap
     return decorator
+
+
+def timeout_handler(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except Timeout:
+            return self._timeout()
+
+    return wrapper
