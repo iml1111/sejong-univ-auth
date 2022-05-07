23fe24fa63c102657791f136c9ae39f887cef4a5
IML <shin10256@gmail.com>
2022-05-07 14:39:33 +0900

    ClassicSession 수집 정보 추가
    - 인증 검증 방식 일부 리팩토링
    - 0.3.2 Tagging

--- sejong_univ_auth/authenticator/classic_session.py
+++ sejong_univ_auth/authenticator/classic_session.py
@@ -1,8 +1,10 @@
 """대양휴머니티칼리지 세션을 통한 로그인 검증"""
 import requests
 from bs4 import BeautifulSoup as bs
+from bs4.element import PageElement
 from sejong_univ_auth.authenticator import Authenticator, AuthResponse
 from sejong_univ_auth.decorator import timeout_handler
+from sejong_univ_auth.exceptions import ParseError
 
 
 class ClassicSession(Authenticator):
@@ -11,26 +13,20 @@ class ClassicSession(Authenticator):
     def authenticate(self, id: str, password: str) -> AuthResponse:
         res = self._session_request(id, password)
         if res.status_code == 200:
-            soup = bs(res.text, 'html.parser')
-            soups = soup.select(
-                'div.contentWrap > ul.tblA > li > dl > dd')
-            if not soups:
-                return self._auth_failed(is_auth=False)
-            major = soups[0].get_text().strip()
-            name = soups[2].get_text().strip()
-            grade = soups[3].get_text().strip()
-            status = soups[4].get_text().strip()
-            return self._success(
-                body={
-                    'name': name,
-                    'major': major,
-                    'grade': grade,
-                    'status': status,
-                }
-            )
+            try:
+                userinfo: dict = self._get_userinfo(res.text)
+            except ParseError as e:
+                if e.expected:
+                    return self._auth_failed(is_auth=False)
+                else:
+                    return self._unknown_issue()
+            except Exception:
+                return self._unknown_issue()
+            
+            return self._success(body=userinfo)
         else:
             return self._unknown_server_error(
-                status_code=response.status_code
+                status_code=res.status_code
             )
 
 
@@ -55,6 +51,47 @@ class ClassicSession(Authenticator):
                 headers=self.header,
                 timeout=self.timeout_sec
             )
+    
+    def _get_userinfo(self, content: str):
+        soup = bs(content, 'html.parser')
+
+        # 사용자 기본 정보 조회
+        soups = soup.select(
+            'div.contentWrap > ul.tblA > li > dl > dd')
+        if not soups:
+            raise ParseError(expected=True)
+        t = self.convert_text
+
+        major = t(soups[0])
+        name = t(soups[2])
+        grade = t(soups[3])
+        status = t(soups[4])
+
+        # 고전 독서 인증 현황 정보
+        soup = soup.select_one('table.listA')
+        if not soups:
+            raise ParseError(expected=True)
+        key_soups = soup.select('thead > tr > th')
+        value_soups = soup.select('tbody > tr > td')
+        read_certification = {
+            t(key): t(value) 
+            for key, value in zip(
+                key_soups[1:5], 
+                value_soups[1:5], 
+            )
+        }
+        
+        return {
+            'major': major,
+            'name': name,
+            'grade': grade,
+            'status': status,
+            'read_certification': read_certification,
+        }
+
+    @staticmethod
+    def convert_text(soup: PageElement) -> str:
+        return soup.get_text().strip()
 
 
 if __name__ == '__main__':
--- sejong_univ_auth/exceptions.py
+++ sejong_univ_auth/exceptions.py
@@ -2,5 +2,17 @@
 
 class InvalidMethod(Exception):
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '"methods" argument must be a single object that inherits from the "Authenticator" class or a list.'
+
+
+class ParseError(Exception):
+
+    def __init__(self, expected: bool):
+        self.expected = expected
+
+    def __str__(self) -> str:
+        if self.expected:
+            return "Failed parse user resourse."
+        else:
+            return "Unknown Website Structure."
